bb56b0940301d9e13cf73f0c41e0c2efef0669d7
Bob1803 <tk.vladimir@gmail.com>
2019-09-05 14:36:56 +0300

    [Corrected styles in the Logo component]

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,29 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-//import { ConfigLogo } from "../../styles/vars";
-//import Radium from "radium";
+import "./logo.less";
 
-//@Radium
 export default class Logo extends React.Component {
   constructor(props) {
     super(props);
-    /*
-    this.styles = {
-      display: "inline-block",
-      color: ConfigLogo.colorLogo,
-      padding: ConfigLogo.padding,
-      fontWeight: ConfigLogo.fontWeight,
-      ':hover': {
-        color: ConfigLogo.colorLogoHover
-      }
-    };
-    */
+   
   }
   render() {
     return (
       <div className="logo__container">
         <Link to="/">
-          <div className="logo">LOGO</div>
+          <div className="logo">CONSTRUCTOR</div>
         </Link>
       </div>
     );
