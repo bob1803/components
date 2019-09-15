1a1cb2747016430922958505c3df7c7de3b00c0e
Bob1803 <tk.vladimir@gmail.com>
2019-09-15 07:44:42 +0300

    [LinkSmall] Added feature in the color switch component. Possible value props color: 'primary', 'secondary'.

--- src/components/link/linkSmall.jsx
+++ src/components/link/linkSmall.jsx
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 import "./linkSmall.less";
 
 export default class LinkSmall extends React.Component {
+  constructor(props) {
+    super(props);
+    this._color = this.props.color || "primary"
+  }
+
   render() {
     return (
       <Link to={this.props.link}>
-        <span className="link-small">{this.props.children}</span>
+        <span className={`link-small link-small__${this._color}`} >{this.props.children}</span>
       </Link>
     );
   }
